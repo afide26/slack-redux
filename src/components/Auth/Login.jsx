5adb5685a96e166b9e8adb7167ada4bdee9d7182
Alan Fidelino <afidedev26@gmail.com>
2019-04-20 21:20:51 +1000

    Style and set-up login form

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,9 +1,121 @@
 import React, { Component } from "react";
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import firebase from "../../firebase";
 
 class Login extends Component {
-  state = {};
+  state = {
+    email: "",
+    password: "",
+    errors: [],
+    loading: false
+  };
+
+  // Display the errors in UI
+  displayErrors = errors => errors.map((e, i) => <p key={i}>{e.message}</p>);
+  handleChange = event => {
+    event.preventDefault();
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.isFormValid(this.state)) {
+      this.setState({ errors: [], loading: true });
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.state.email, this.state.password)
+        .then(signedInUser => {
+          this.setState({ loading: false });
+          console.log(signedInUser);
+        })
+        .catch(err => {
+          console.error(err);
+          this.setState({
+            errors: [...this.state.errors, err],
+            loading: false
+          });
+        });
+    }
+  };
+
+  // create a new isFormValid function
+  isFormValid = ({ email, password }) => email && password;
+  // Check if the error is related to email, username or password mismatch
+  handleInputErrors = (errors, inputName) => {
+    return errors.some(error => error.message.toLowerCase().includes(inputName))
+      ? "error"
+      : "";
+  };
+
   render() {
-    return <div>Login</div>;
+    // Destructure state properties and pass them as values to make the form a controlled component
+    const { email, password, errors, loading } = this.state;
+    return (
+      <Grid textAlign="center" verticalAlign="middle" className="app">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h1" icon color="violet" textAlign="center">
+            <Icon name="code branch" color="orange" />
+            Login to SlackDevChat
+          </Header>
+          <Form size="large" onSubmit={this.handleSubmit}>
+            <Segment stacked>
+              <Form.Input
+                fluid
+                autoComplete="true"
+                name="email"
+                icon="mail"
+                iconPosition="left"
+                placeholder="Email address"
+                onChange={this.handleChange}
+                type="email"
+                value={email}
+                className={this.handleInputErrors(errors, "email")}
+              />
+              <Form.Input
+                fluid
+                autoComplete="true"
+                name="password"
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                onChange={this.handleChange}
+                type="password"
+                value={password}
+                className={this.handleInputErrors(errors, "password")}
+              />
+              <Button
+                disabled={loading}
+                className={loading ? "loading" : ""}
+                color="violet"
+                fluid
+                size="large"
+              >
+                Submit
+              </Button>
+            </Segment>
+            <Message>
+              Don't have an account?&nbsp;
+              <Link to="/register">Register</Link>
+            </Message>
+          </Form>
+          {errors.length > 0 && (
+            <Message error>
+              <h3>Error</h3>
+              {this.displayErrors(errors)}
+            </Message>
+          )}
+        </Grid.Column>
+      </Grid>
+    );
   }
 }
 
