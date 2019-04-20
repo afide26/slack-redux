602f2da602bc5ef70324af7aacc060de7251cbd6
Alan Fidelino <afidedev26@gmail.com>
2019-04-20 15:37:35 +1000

    Style the register component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+
 import App from "./components/App";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./Auth/Login";
 import Register from "./Auth/Register";
+import "semantic-ui-css/semantic.min.css";
 
 import registerServiceWorker from "./registerServiceWorker";
 
