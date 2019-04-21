48867b9ae56681679e205fe57ba24fd1cd161c46
Alan Fidelino <afidedev26@gmail.com>
2019-04-21 16:34:52 +1000

    Scaffolding the app with ColorPanel, Messages, SidePanel and MetaPanel

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,26 @@
-import React, { Component } from "react";
-import "./App.css";
-import { Link } from "react-router-dom";
+import React from "react";
+import { Grid } from "semantic-ui-react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1> Hello React </h1>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-      </div>
-    );
-  }
-}
+import ColorPanel from "./ColorPanel/ColorPanel";
+import SidePanel from "./SidePanel/SidePanel";
+import Messages from "./Messages/Messages";
+import MetaPanel from "./MetaPanel/MetPanel";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <Grid className="app" columns="equal" style={{ background: "#eee" }}>
+      <ColorPanel />
+      <SidePanel />
+      <Grid.Column style={{ marginLeft: "320p" }}>
+        <Messages />
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <MetaPanel />
+      </Grid.Column>
+    </Grid>
+  );
+};
 
 export default App;
